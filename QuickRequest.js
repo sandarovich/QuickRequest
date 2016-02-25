3d47b8900b168d693e829d5f092b67ebade11aaf
Sandarovich <sandarovich1@ukr.net>
2016-02-25 16:00:09 +0200

    NodeJs server side code cleanup

--- QuickRequest.js
+++ QuickRequest.js
@@ -71,10 +71,11 @@ passport.use(new passportlocal.Strategy(function(username, password, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
       
-      // Find the user by username.  If there is no user with the given
-      // username, or the password is not correct, set the user to `false` to
+      // Find the user by user name.  If there is no user with the given
+      // user name, or the password is not correct, set the user to `false` to
       // indicate failure and set a flash message.  Otherwise, return the
       // authenticated `user`.
+	  
       findByUsername(username, function(err, user) {
         if (err) { return done(err); }
         if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
@@ -118,25 +119,15 @@ app.get('/login', function(req, res){
 app.get('/template', function(req, res){
   res.send(searchTemplate(req.query.templateID));
 });   
-/*
-app.post('/login', passport.authenticate('local', //function(req, res) {
-	{ failureRedirect: '/login',
-	successRedirect: '/',
-	failureFlash : true
-	//res.redirect('/');
-}));
-*/
 
 app.post('/login', passport.authenticate('local', //function(req, res) {
 	{ failureRedirect: '/login',
 	successRedirect: '/',
 	failureFlash : true
-	//res.redirect('/');
 }));
 
 app.get('/esend', function(req,res){
-  //var d = new Date();
-  var message = {
+   var message = {
       from: req.query.from,
       to : req.query.to,
       cc : req.query.cc,
@@ -146,7 +137,6 @@ app.get('/esend', function(req,res){
       },
       text : req.query.text
   };
-  //console.log('[' + moment().format('DD.MM.YYYY HH:mm:ss') +'] Sending Mail');
 
   transport.sendMail(message, function(error, info) {
     if (error) {
@@ -176,11 +166,8 @@ app.post('/login', function(req, res, next) {
   })(req, res, next);
 });
 
-
-
-
 app.get('/logout', function(req,res) {
-	req.logout(); // Passport ficha
+	req.logout(); // Passport functionality
 	res.redirect('/');
 });
 
@@ -190,70 +177,70 @@ app.listen(port, function() {
 	console.log('[Ok] - Server is running on http://uaievad7:' + port + '/');
 });
 
-
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
 }
+
 function searchTemplate(code) {
 switch (code) {
     case '1':
-      output = 'Пакування пошти' + '\n'
+      result = 'Пакування пошти' + '\n'
         + 'Підготовка техніки/листа до транспортування адресату. Біркування пошти ' 
         + 'відповідною наклейкою та позначеннями. Обертання пакунка скотчем.'
         + 'Передача пошти секретарю рецепції.';
     break
     case '2': 
-      output = 'Забір/розбір пошти' + '\n'
+      result = 'Забір/розбір пошти' + '\n'
         + 'Прийом пошти від секретарю рецепції. Сортування. Розпаковка пакунка. ' 
         + 'Ідентифікація листа чи пакунка.'
         + 'Передача відповідальній особі.';
     break
     case '3':
-     output = 'Адіністративна робота.' + '\n'
+     result = 'Адіністративна робота.' + '\n'
         + 'Рахунки. Акти. Договора. Підготовка документації. Збір підписів. Простановка мокрої печатки.' 
         + ' Передача/забір документів у відповідних служб. Уточнення, прискорення проплати в Finance.'
         + ' Проведення інвентиризацій техніки. Оформлення списань.'
         ;
     break
     case '4':
-     output = 'Адіністративна робота.PO' + '\n'
+     result = 'Адіністративна робота.PO' + '\n'
         + ' Питання створення нового матеріалу та вендору. Cтворення Purchase requesition. ' 
         + ' Отримання схвалення керівником IT. Комунікація с PR відділом, щодо створення'
         + ' PO. Контроль і пітвердження на рівнях PO.FA.CFO. Комунікація постачальнику.';
     break
      case '5':
-     output = 'OFF.Консультація' + '\n'
+     result = 'OFF.Консультація' + '\n'
         + 'Ідентифікація звернення. Надання нагальної і вичерпної ' 
         + 'інформації, щодо роботи системи OFF. Допомога у підключенні Datasource.'
         + 'Пошук та виправлення помилок. Нагальне оновлення і завантаження інформації.'
         + 'Складання технчіної документації та листів з поясненнями';
     break
     case '6':
-      output = 'TVonGo.Консультація' + '\n'
+      result = 'TVonGo.Консультація' + '\n'
         + 'Ідентифікація звернення. Надання нагальної і вичерпної ' 
         + 'інформації, щодо роботи системи  TV on Go. Виправлення помилок у контенті.'
         + 'Перезавантаження серверу. Налаштування панелей та скриптів.'
         + 'Складання технчіної документації та листів з поясненнями';
     break
     case '7':
-      output = 'HHT. iPAD.' + '\n'
+      result = 'HHT. iPAD.' + '\n'
         + 'Заміна, оперативний ремонт техніки, видача нової техніки ' 
         + 'та комплектуючих. Складання актів прийому-передачі.';
     break
     case '8':
-      output = 'Постачальники' + '\n'
+      result = 'Постачальники' + '\n'
         + 'Робота с постачальник. Комунікації. Супроводження.' 
         + 'Вирішення їх проблем.';
     break
     case '9':
-      output = 'Інші питання' + '\n'
+      result = 'Інші питання' + '\n'
         + 'Любий спектр питань. Наприклад: Картриджі. Патчкорди, телефонія, комплектуючі,'
         + ' зв\'язок, доступи. Просто не працює. Користувач бажає поспілкуватися з IT. '
         ;
     break
-    default: output='';
+    default: result='';
     break
   }
-  return output;
+  return result;
 }
